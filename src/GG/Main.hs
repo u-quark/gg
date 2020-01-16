9b0912420d1fb7c123c9e5031d4f1a90536442bf
u_quark <eleftherios.kritikos@gmail.com>
2020-01-16 22:51:29 +0000

    Move main under GG.Main package

--- src/GG/Main.hs
+++ src/GG/Main.hs
@@ -15,9 +15,23 @@
   You should have received a copy of the GNU General Public License
   along with gg.  If not, see <https://www.gnu.org/licenses/>.
 -}
-module Main where
+module GG.Main
+  ( main
+  ) where
 
-import qualified GG.Main (main)
+import           Brick.BChan (newBChan)
+import qualified GG.Repo     as R
+import qualified GG.State    as S
+import           GG.Timers   (initTimers)
+import qualified GG.UI       as UI
+import           Prelude     hiding (head)
 
 main :: IO ()
-main = GG.Main.main
+main = do
+  repo <- R.readRepository
+  (head, headCommit) <- R.readRepoState repo
+  (tailCommits, contCommit) <- R.readNCommits 999 headCommit
+  bChan <- newBChan 10
+  timers <- initTimers S.Tick 100000 bChan
+  commitsState <- mapM R.readCommit (headCommit : tailCommits)
+  UI.main bChan $ S.initState repo contCommit head commitsState timers
