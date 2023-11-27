e359b2ee107d902c3cd38b4a73110cf242f005a5
u_quark <eleftherios.kritikos@gmail.com>
2023-11-27 20:08:53 +0000

    Add e2e tests for navigation

--- e2e-tests/test_e2e.py
+++ e2e-tests/test_e2e.py
@@ -46,9 +46,13 @@ def tmp_repo(tmp_path):
     yield tmp_path
 
 def get_runner(tmp_repo):
-    gg_path = Path(environ["GG_PATH"]).resolve().absolute()
+    gg_path = str(Path(environ["GG_PATH"]).resolve().absolute())
+    with open(tmp_repo / ".tmux.conf", "w") as fd:
+        fd.write("""
+set-option -g mouse on
+        """)
     runner = hecate.Runner(
-        "sh", "-c", f"cd {tmp_repo}; {gg_path}",
+        gg_path,
         width=120, height=36,
         env={
             "HOME": tmp_repo,
@@ -56,7 +60,9 @@ def get_runner(tmp_repo):
             "GIT_COMMITTER_DATE": "1970-01-01T00:00:00.000+00:00",
             "TERM": "tmux-256color",
             "COLORTERM": "truecolor",
-        }
+        },
+        cwd=str(tmp_repo),
+        tmux_conf=tmp_repo / ".tmux.conf",
     )
     sleep(0.2)
     return runner
@@ -68,7 +74,7 @@ class ScreenshotChecker:
 
     def check(self, runner):
         __tracebackhide__ = True
-        sleep(0.2)
+        sleep(0.1)
         screenshot = runner.screenshot_with_escape_sequences()
         self.count += 1
         screenshots_dir = Path("./screenshots")
@@ -88,6 +94,15 @@ class ScreenshotChecker:
                 fd.write(screenshot)
             print(f"Saved new screenshot to {new_filepath}")
             pytest.fail(f"Screenshot #{self.count:03} differs, test: {self.test_name}")
+
+def escape_sequence(sequence):
+    return ["-H", "1b"] + list(f"{ord(s):x}" for s in sequence)
+
+def mouse_wheel_down(x, y):
+    return escape_sequence(f"[<65;{x};{y}M")
+
+def mouse_wheel_up(x, y):
+    return escape_sequence(f"[<64;{x};{y}M")
 
 @pytest.fixture
 def screenshot_checker(request):
@@ -120,5 +135,75 @@ def test_basic_functionality(tmp_repo, screenshot_checker):
         runner.press("enter")
         screenshot_checker.check(runner)
         runner.press("q")
+        runner.press("q")
+        runner.await_exit()
+
+def test_navigation(tmp_repo, screenshot_checker):
+    for i in range(100):
+        git("commit", "-m", f"commit {i}", "--allow-empty", repo=tmp_repo)
+    with open(tmp_repo / "test", "w") as fd:
+        for i in range(100):
+            fd.write(f"test {i}\n")
+    git("add", "test", repo=tmp_repo)
+    git("commit", "-m", "test", repo=tmp_repo)
+    with get_runner(tmp_repo) as runner:
+        # Enter commit diff
+        screenshot_checker.check(runner)
+        runner.press("enter")
+        screenshot_checker.check(runner)
+        # Viewport scrolling
+        runner.press("down")
+        screenshot_checker.check(runner)
+        runner.press("up")
+        screenshot_checker.check(runner)
+        runner.press("pagedown")
+        screenshot_checker.check(runner)
+        runner.press("pageup")
+        screenshot_checker.check(runner)
+        runner.press("end")
+        screenshot_checker.check(runner)
+        runner.press("home")
+        screenshot_checker.check(runner)
+        runner.press("j")
+        screenshot_checker.check(runner)
+        runner.press("k")
+        screenshot_checker.check(runner)
+        runner.press("c-f")
+        screenshot_checker.check(runner)
+        runner.press("c-b")
+        screenshot_checker.check(runner)
+        runner.press("G")
+        screenshot_checker.check(runner)
+        runner.press("g")
+        screenshot_checker.check(runner)
+        runner.press(*mouse_wheel_down(1, 22))
+        screenshot_checker.check(runner)
+        runner.press(*mouse_wheel_up(1, 22))
+        screenshot_checker.check(runner)
+        # Exit commit diff
+        runner.press("q")
+        screenshot_checker.check(runner)
+        # Commit list scrolling
+        runner.press("down")
+        screenshot_checker.check(runner)
+        runner.press("pagedown")
+        screenshot_checker.check(runner)
+        runner.press("end")  # NO-OP
+        screenshot_checker.check(runner)
+        runner.press("home")
+        screenshot_checker.check(runner)
+        runner.press("j")
+        screenshot_checker.check(runner)
+        runner.press("c-f")
+        screenshot_checker.check(runner)
+        runner.press("G")  # NO-OP
+        screenshot_checker.check(runner)
+        runner.press("g")
+        screenshot_checker.check(runner)
+        runner.press(*mouse_wheel_down(1, 1))
+        screenshot_checker.check(runner)
+        runner.press(*mouse_wheel_up(1, 1))
+        screenshot_checker.check(runner)
+        # Exit
         runner.press("q")
         runner.await_exit()
